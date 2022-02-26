724e1f37bfcf5359147deab3318effa0eba950e6
Gabriel Lacroix <lacroixgabriel@gmail.com>
2022-02-25 23:46:06 -0500

    Cool frontend stuff

--- proximitychat/App.tsx
+++ proximitychat/App.tsx
@@ -1,10 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import { io } from "socket.io-client";
 
 export default function App() {
+  const [text, onChangeText] = React.useState("");
+  const socket = io("https://proximitychat.glcrx.com");
+  function onSend() {
+    if (text) {
+      socket.emit("chat message", text);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <TextInput
+        value={text}
+        onChangeText={onChangeText}
+        placeholder="Send a message"
+      />
+      <Button
+        title="Send"
+        onPress={onSend}
+        disabled={text.trim().length === 0}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +32,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
