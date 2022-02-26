aa585daedbdad6f9d6ce43c4c65ebfa82a028cab
edwin <edwin99@live.ca>
2022-02-26 00:45:52 -0500

    integrate map sdk

--- proximitychat/App.tsx
+++ proximitychat/App.tsx
@@ -1,9 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default function App() {
+  const [mapRegion, setmapRegion] = useState({
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
   return (
     <View style={styles.container}>
+      <MapView
+      region={mapRegion}
+      style={{ alignSelf: 'stretch', height: '100%' }}>
+
+      </MapView>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
