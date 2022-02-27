a71ade5d8e9cb7b51ff84760bfb0ed3d0fe152d9
Gabriel Lacroix <lacroixgabriel@gmail.com>
2022-02-26 22:15:49 -0500

    Try and fix shit

--- proximitychat/App.tsx
+++ proximitychat/App.tsx
@@ -12,22 +12,19 @@ import {
   Keyboard,
   TouchableWithoutFeedback,
 } from "react-native";
-import { Chip,  Modal, Button,
-  Portal,
-  Provider} from "react-native-paper";
+import { Chip, Modal, Button, Portal, Provider } from "react-native-paper";
 import io from "socket.io-client";
 import * as GeoLocation from "expo-location";
 import emojis from "./Emojis";
 
-
 export default function App() {
   //modal
   const [visible, setVisible] = React.useState(true);
-  const [name, setId] = React.useState("");
+  const [name, setName] = React.useState("");
   const [notSet, setNotSet] = React.useState(true);
   const showModal = () => setVisible(true);
   const hideModal = () => setVisible(false);
-  const containerStyle = {backgroundColor: 'white', padding: 20};
+  const containerStyle = { backgroundColor: "white", padding: 20 };
 
   const socket = io("https://proximitychat.glcrx.com");
   const [location, setLocation] = useState<any>(null);
@@ -37,38 +34,39 @@ export default function App() {
     latitudeDelta: 0.0024,
     longitudeDelta: 0.00011,
   });
-  const [ownMessage, setOwnMessage] = React.useState("")
+  const [ownMessage, setOwnMessage] = React.useState("");
   const [text, setText] = React.useState("");
-  const [emojiMapping, setEmojiMapping] = React.useState(new Map())
   const [users, setUsers] = useState<UserInfo[]>([]);
   const [messages, setMessages] = useState<any>({});
 
   useEffect(() => {
     socket.once("local message", (message: UserMessage) => {
-      let nextMessages = {...messages};
+      let nextMessages = { ...messages };
       // console.log(JSON.stringify(nextMessages));
-      nextMessages[message.id] =  message.message;
+      nextMessages[message.id] = message.message;
       // console.log(nextMessages)
       setMessages(nextMessages);
       // console.log(JSON.stringify(messages));
       setTimeout(() => {
-        let nextMessages = {...messages};
+        let nextMessages = { ...messages };
         if (nextMessages[message.id] === message.message) {
           delete nextMessages[message.id];
           setMessages(nextMessages);
         }
       }, 15000);
     });
-  })
+  });
 
   socket.on("locations", (locations: UserInfo[]) => {
     setUsers(locations);
   });
 
   const toEmoji = (name: String): String => {
-    const key = name.split("").reduce((acc, c) => c.charCodeAt(0) * acc, 1) % emojis.length
-    return emojis[key]
-  }
+    const key =
+      name.split("").reduce((acc, c) => c.charCodeAt(0) * acc, 1) %
+      emojis.length;
+    return emojis[key];
+  };
 
   useEffect(() => {
     (async () => {
@@ -99,11 +97,11 @@ export default function App() {
   const handleSendMessage = () => {
     if (text.trim().length > 0) {
       socket.emit("local message", name, text);
-      setOwnMessage(text)
+      setOwnMessage(text);
       setText("");
       setTimeout(() => {
-        setOwnMessage("")
-      }, 15000)
+        setOwnMessage("");
+      }, 15000);
     }
   };
 
@@ -141,95 +139,125 @@ export default function App() {
   if (notSet) {
     return (
       <Provider>
-    <Portal>
-         <Modal dismissable={false} visible={visible} onDismiss={hideModal} contentContainerStyle={containerStyle}>
-         <TextInput
-          value={textName}
-          label="textName"
-          onChangeText={textName => setId(textName)}
-          />
-      <Button mode="contained" onPress={() => {setNotSet(false); hideModal(); console.log(name)}}>
-    Chat
-  </Button>
-         </Modal>
-      </Portal>
-    </Provider>
-    )
-  }
-  else{
+        <Portal>
+          <Modal
+            dismissable={false}
+            visible={visible}
+            onDismiss={hideModal}
+            contentContainerStyle={containerStyle}
+          >
+            <TextInput
+              value={textName}
+              onChangeText={(textName) => setName(textName)}
+            />
+            <Button
+              mode="contained"
+              onPress={() => {
+                setNotSet(false);
+                hideModal();
+                console.log(name);
+              }}
+            >
+              Chat
+            </Button>
+          </Modal>
+        </Portal>
+      </Provider>
+    );
+  } else {
     return (
-          <KeyboardAvoidingView
-            behavior={Platform.OS === "ios" ? "padding" : "height"}
-            style={styles.container}
-          > 
-            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-              <View style={styles.inner}>
-                <MapView
-                  zoomEnabled={false}
-                  pitchEnabled={false}
-                  scrollEnabled={false}
-                  region={mapRegion}
-                  style={styles.map}
-                >
-                  {location && (
-                    <View>
-                      <Marker
-                        style={{justifyContent: "center", alignItems: "center"}}
-                        coordinate={{
-                          latitude: location.coords.latitude,
-                          longitude: location.coords.longitude,
-                      }}>
-                        { ownMessage ?
-                        <View style={{backgroundColor: "#fefefe", borderRadius:12, padding: 5}}>
-                          <Text numberOfLines={5} style={{maxWidth:100}}>{ownMessage}</Text>
-                        </View>
-                        :
-                        <View></View>
-                        }
-                        <Text style={{fontSize:35}}>{toEmoji(name)}</Text>
-                      </Marker>
-                    </View>
-                  )}
-                  {users.map((user: UserInfo, index: number) => (
-                    <View key={index}>
-                      <Marker
-                        coordinate={{
-                          latitude: user.location.latitude,
-                          longitude: user.location.longitude,
-                        }}
-                        style={{justifyContent: "center", alignItems: "center"}}
-                      >
-                      { messages[user.id] ?
-                      <View style={{left:-25, backgroundColor: "#fefefe", borderRadius:12, padding: 5}}>
-                        <Text numberOfLines={5} style={{maxWidth:100}}>{messages[user.id]}</Text>
-                      </View>
-                      :
-                      <View></View>
-                      }       
-                      </Marker>  
-                      <Text style={{fontSize:35}}>{toEmoji(user.id)}</Text>
-                    </View>                       
-                  ))}
-                </MapView>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.container}
+      >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.inner}>
+            <MapView
+              zoomEnabled={false}
+              pitchEnabled={false}
+              scrollEnabled={false}
+              region={mapRegion}
+              style={styles.map}
+            >
+              {location && (
                 <View>
-                  <View style={styles.inputContainer}>
-                    <TextInput
-                      value={text}
-                      placeholder="Send a message"
-                      onChangeText={setText}
-                      style={styles.input}
-                    />
-                    <Button
-                      disabled={text.trim().length === 0}
-                      onPress={handleSendMessage}
-                    >Go</Button>
-                    <View style={{ width: 10 }}></View>
-                  </View>
+                  <Marker
+                    style={{ justifyContent: "center", alignItems: "center" }}
+                    coordinate={{
+                      latitude: location.coords.latitude,
+                      longitude: location.coords.longitude,
+                    }}
+                  >
+                    {ownMessage ? (
+                      <View
+                        style={{
+                          backgroundColor: "#fefefe",
+                          borderRadius: 12,
+                          padding: 5,
+                        }}
+                      >
+                        <Text numberOfLines={5} style={{ maxWidth: 100 }}>
+                          {ownMessage}
+                        </Text>
+                      </View>
+                    ) : (
+                      <View></View>
+                    )}
+                    <Text style={{ fontSize: 35 }}>{toEmoji(name)}</Text>
+                  </Marker>
                 </View>
-                <View style={{ height: Platform.OS === "ios" ? 10 : 0 }}></View>
+              )}
+              {users.map((user: UserInfo, index: number) => (
+                <View key={index}>
+                  <Marker
+                    coordinate={{
+                      latitude: user.location.latitude,
+                      longitude: user.location.longitude,
+                    }}
+                    style={{ justifyContent: "center", alignItems: "center" }}
+                  >
+                    {messages[user.id] ? (
+                      <View
+                        style={{
+                          left: -25,
+                          backgroundColor: "#fefefe",
+                          borderRadius: 12,
+                          padding: 5,
+                        }}
+                      >
+                        <Text numberOfLines={5} style={{ maxWidth: 100 }}>
+                          {messages[user.id]}
+                        </Text>
+                      </View>
+                    ) : (
+                      <View></View>
+                    )}
+                  </Marker>
+                  <Text style={{ fontSize: 35 }}>{toEmoji(user.id)}</Text>
+                </View>
+              ))}
+            </MapView>
+            <View>
+              <View style={styles.inputContainer}>
+                <TextInput
+                  value={text}
+                  placeholder="Send a message"
+                  onChangeText={setText}
+                  style={styles.input}
+                />
+                <Button
+                  disabled={text.trim().length === 0}
+                  onPress={handleSendMessage}
+                >
+                  Go
+                </Button>
+                <View style={{ width: 10 }}></View>
               </View>
-            </TouchableWithoutFeedback>
-          </KeyboardAvoidingView>
+            </View>
+            <View style={{ height: Platform.OS === "ios" ? 10 : 0 }}></View>
+          </View>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
     );
   }
 }
